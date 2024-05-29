4fbaffb00249c8a182144af93e45d8bb61d4f695
202282-NguyenTu <sing19022001@gmail.com>
2024-05-29 10:09:14 +0700

    clear main

--- src/main/java/com/example/demoMyBatis/config/MybatisUtil.java
+++
@@ -1,9 +0,0 @@
-package com.example.demoMyBatis.config;
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan("com.example.demoMyBatis.mapper")
-public class MybatisUtil {
-    
-}
