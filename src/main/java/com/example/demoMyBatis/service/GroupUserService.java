8d418f6b0648a28d348a8f08c3c4362f34211b1a
nguyentu1902 <77284316+nguyentu1902@users.noreply.github.com>
2025-03-14 08:23:05 +0700

    Update GroupUserService.java

--- src/main/java/com/example/demoMyBatis/service/GroupUserService.java
+++ src/main/java/com/example/demoMyBatis/service/GroupUserService.java
@@ -1,47 +1,47 @@
-package com.example.demoMyBatis.service;
+// package com.example.demoMyBatis.service;
 
-import java.util.HashSet;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+// import java.util.HashSet;
+// import java.util.List;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
+// import org.springframework.transaction.annotation.Transactional;
 
-import com.example.demoMyBatis.mapper.GroupUsersMapper;
-import com.example.demoMyBatis.model.Group;
+// import com.example.demoMyBatis.mapper.GroupUsersMapper;
+// import com.example.demoMyBatis.model.Group;
 
-@Service
-public class GroupUserService {
-    @Autowired
-    private GroupUsersMapper groupUsersMapper;
+// @Service
+// public class GroupUserService {
+//     @Autowired
+//     private GroupUsersMapper groupUsersMapper;
 
-    public List<Group> getAllGroups() {
-        return groupUsersMapper.getAllGroups();
-    }
+//     public List<Group> getAllGroups() {
+//         return groupUsersMapper.getAllGroups();
+//     }
 
-    public List<Group> getAllGroupByNameContains(String name) {
-        return groupUsersMapper.getAllGroupByNameContains(name);
-    }
+//     public List<Group> getAllGroupByNameContains(String name) {
+//         return groupUsersMapper.getAllGroupByNameContains(name);
+//     }
 
-    public Group getGroupById(int id) {
-        return groupUsersMapper.getGroupById(id);
-    }
+//     public Group getGroupById(int id) {
+//         return groupUsersMapper.getGroupById(id);
+//     }
 
-    public void createGroup(Group group) {
-        groupUsersMapper.createGroup(group);
-    }
+//     public void createGroup(Group group) {
+//         groupUsersMapper.createGroup(group);
+//     }
 
-    public void updateGroup(Group group) {
-        groupUsersMapper.updateGroup(group);
-    }
+//     public void updateGroup(Group group) {
+//         groupUsersMapper.updateGroup(group);
+//     }
 
-    public void deleteGroup(int id) {
-        groupUsersMapper.deleteGroup(id);
-    }
+//     public void deleteGroup(int id) {
+//         groupUsersMapper.deleteGroup(id);
+//     }
 
-    @Transactional
-    public void updateUsersForGroup(HashSet<Integer> usersId, int groupId){
-        groupUsersMapper.deleteUsersForGroup(groupId);
-        if(usersId != null && !usersId.isEmpty())
-            groupUsersMapper.insertUsersForGroup(usersId, groupId);
-    }
-}
+//     @Transactional
+//     public void updateUsersForGroup(HashSet<Integer> usersId, int groupId){
+//         groupUsersMapper.deleteUsersForGroup(groupId);
+//         if(usersId != null && !usersId.isEmpty())
+//             groupUsersMapper.insertUsersForGroup(usersId, groupId);
+//     }
+// }
