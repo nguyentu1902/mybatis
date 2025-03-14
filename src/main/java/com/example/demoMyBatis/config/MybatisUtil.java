2b76b1e2e2becb3528d9d06e8b243d0c159f7256
nguyentu1902 <77284316+nguyentu1902@users.noreply.github.com>
2025-03-14 08:20:42 +0700

    Update MybatisUtil.java

--- src/main/java/com/example/demoMyBatis/config/MybatisUtil.java
+++ src/main/java/com/example/demoMyBatis/config/MybatisUtil.java
@@ -2,11 +2,9 @@ package com.example.demoMyBatis.config;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@MapperScan("com.example.demoMyBatis.mapper")
+// @Configuration
+// @MapperScan("com.example.demoMyBatis.mapper")
 public class MybatisUtil {
     
 }
 
-
-// cc cc
