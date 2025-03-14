56e1cde5913d23a4924fe90784a984b3a4d4214d
nguyentu1902 <77284316+nguyentu1902@users.noreply.github.com>
2025-03-14 08:23:15 +0700

    Update UserService.java

--- src/main/java/com/example/demoMyBatis/service/UserService.java
+++ src/main/java/com/example/demoMyBatis/service/UserService.java
@@ -1,41 +1,41 @@
-package com.example.demoMyBatis.service;
+// package com.example.demoMyBatis.service;
 
-import java.util.List;
+// import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
 
-import com.example.demoMyBatis.mapper.UserMapper;
-import com.example.demoMyBatis.model.User;
+// import com.example.demoMyBatis.mapper.UserMapper;
+// import com.example.demoMyBatis.model.User;
 
-@Service
-public class UserService{
+// @Service
+// public class UserService{
 
-    @Autowired
-    private UserMapper userMapper;
+//     @Autowired
+//     private UserMapper userMapper;
 
-    public List<User> getAllUsers(){
-        return userMapper.getAllUsers();
-    }
+//     public List<User> getAllUsers(){
+//         return userMapper.getAllUsers();
+//     }
 
-    public User getUserById(int id){
-        return userMapper.getUserById(id);
-    }
+//     public User getUserById(int id){
+//         return userMapper.getUserById(id);
+//     }
 
-    public void createUser(User user){
-        userMapper.insertUser(user);
-    }
+//     public void createUser(User user){
+//         userMapper.insertUser(user);
+//     }
 
-    public void updateUser(User user){
-        userMapper.updateUser(user);
-    }
+//     public void updateUser(User user){
+//         userMapper.updateUser(user);
+//     }
 
-    public void deleteUser(int id){
-        userMapper.deleteUser(id);
-    }
+//     public void deleteUser(int id){
+//         userMapper.deleteUser(id);
+//     }
 
-    public List<User> getAllUserByNameContains(String name){
-        return userMapper.getAllUserByNameContains(name);
-    }
+//     public List<User> getAllUserByNameContains(String name){
+//         return userMapper.getAllUserByNameContains(name);
+//     }
 
-}
+// }
